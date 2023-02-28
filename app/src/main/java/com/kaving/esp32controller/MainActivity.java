bc24a00f25251ca8d4b2d11e6d3503cc590cada5
KavinTheG <kavin.balag@gmail.com>
2023-02-27 21:12:29 -0500

    MainActivity will post angle and strength of joystick to ESP32 Endpoint

--- app/src/main/java/com/kaving/esp32controller/MainActivity.java
+++ app/src/main/java/com/kaving/esp32controller/MainActivity.java
@@ -3,8 +3,19 @@ package com.kaving.esp32controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.jacksonandroidnetworking.JacksonParserFactory;
+
+import org.json.JSONObject;
 
 import io.github.controlwear.virtual.joystick.android.JoystickView;
+import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +24,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        JoystickView joystickView = findViewById(R.id.joystick);
+        // Networking Configuration
+        AndroidNetworking.initialize((getApplicationContext()));
 
+        // Adding a Network Interceptor for Debugging purpose
+        OkHttpClient okHttpClient = new OkHttpClient() .newBuilder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+        AndroidNetworking.initialize(getApplicationContext(),okHttpClient);
+
+        AndroidNetworking.setParserFactory(new JacksonParserFactory());
+
+        // Declaring components on activity
+        // joystick component
+        JoystickView joystickView = findViewById(R.id.joystick);
+        // Displays angle of joystick
+        TextView angleText = findViewById(R.id.angle_text);
+        // Displays strength of joystick
+        TextView strengthText = findViewById(R.id.strength_text);
+
+        joystickView.setOnMoveListener((angle, strength) -> {
+
+            // Send joystick data to the ESP32 endpoint
+            AndroidNetworking.post("http://192.168.4.1/joystickdata?angle=" + angle +
+                    "&strength=" + strength).build();
+
+        });
     }
 }
